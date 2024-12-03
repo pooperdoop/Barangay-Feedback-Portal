738b77bbc3d982aef043a69e363dd0c5d849bd81
Maaku3 <markjacinto58@gmail.com>
2024-12-04 02:10:29 +0800

    done na conflict naatos kona
    finish responsiveness

--- JS(sidebar).js
+++ JS(sidebar).js
@@ -2,18 +2,21 @@
 const homeLi = document.getElementById("home");
 const feedbackLi = document.getElementById("feedback");
 const accountsLi = document.getElementById("accounts");
+const settingsLi = document.getElementById("settings");
 const logoutLi = document.getElementById("logout");
 
 // Get the images by their IDs
 const homeImg = document.getElementById("home-img");
 const feedbackImg = document.getElementById("feedback-img");
 const accountsImg = document.getElementById("accounts-img");
+const settingsImg = document.getElementById("settings-img");
 const logoutImg = document.getElementById("logout-img");
 
 // Define the hover images
 const homeHover = "Icons/home2.png";
 const feedbackHover = "Icons/transfer2.png";
 const accountsHover = "Icons/account2.png";
+const settingsHover = "Icons/settings2.png";
 const logoutHover = "Icons/logout2.png";
 
 // Add event listeners for hover effect on each li
@@ -36,6 +39,13 @@ accountsLi.addEventListener("mouseenter", () => {
 });
 accountsLi.addEventListener("mouseleave", () => {
     accountsImg.src = "Icons/account1.png"; // Reset image when hover ends
+});
+
+settingsLi.addEventListener("mouseenter", () => {
+    settingsImg.src = settingsHover; // Change the image on hover
+});
+settingsLi.addEventListener("mouseleave", () => {
+    settingsImg.src = "Icons/settings1.png"; // Reset image when hover ends
 });
 
 logoutLi.addEventListener("mouseenter", () => {
